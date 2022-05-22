fdf449d2ab7ab93bdd31a32bdff8314042b800ab
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2022-05-23 00:40:19 +0200

    fb compatible link sharing

--- src/logic/url-encoder.ts
+++ src/logic/url-encoder.ts
@@ -1,6 +1,7 @@
 import BaseX from 'base-x'
 
-const URL_SAFE_ALPHABET = '#!$%&\'()*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~'
+//const URL_SAFE_ALPHABET = '#!$%&\'()*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~'
+const URL_SAFE_ALPHABET = '#!$%&\'*+-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~'
 const BS90 = BaseX(URL_SAFE_ALPHABET)
 
 export const iFactor = Math.log(256) / Math.log(URL_SAFE_ALPHABET.length) // shows the increase in length from the input data
